d5f63b4e3b12f86ca733ed8e83e73120e664dac5
Nathan <nathanjwtx@nathanjwtx.io>
2018-09-10 13:08:30 -0500

    Changed index.html to look at hangman script. Added a listener to capture keybaord guesses

--- hangman.js
+++ hangman.js
@@ -8,31 +8,41 @@ const HangmanGame = function (word, guesses) {
 }
 
 
-HangmanGame.prototype.GetPuzzle = function (letter) {
+HangmanGame.prototype.GetPuzzle = function () {
     // create array of '*' on first guess
     if (this.guessedLetters.length === 0) {
-        this.letters.forEach(letter => {
-            letter === ' ' ? this.guess.push(' ') : this.guess.push('*')
+        this.letters.forEach(l => {
+            l === ' ' ? this.guess.push(' ') : this.guess.push('*')
         })
     }
+}
+
+HangmanGame.prototype.MakeGuess = function (letter) {
+    // Alternative method: create a boolean variable to test unique/correct guess then use the boolean variables in the if statements
+    // const isUnique = !this.guessedLetters.includes(letter.toLowerCase())
+
+    // Test if already incorrect guess or guessed
+    if (!this.letters.includes(letter.toLowerCase()) || this.guessedLetters.includes(letter.toLowerCase())) {
+        this.guessesLeft -= 1
+    }
     // Add used letters to guessedLetters array
-    this.guessedLetters.push(letter)
-    
+    if (!this.guessedLetters.includes(letter.toLowerCase())) {
+        this.guessedLetters.push(letter.toLowerCase())
+    }
     for (i = 0; i < this.letters.length; i++) {
         if (this.guess[i] === '*' && letter === this.letters[i]) {
-            this.guess[i] = letter
+            this.guess[i] = letter.toLowerCase()
         }
     }
     console.log(this.guess.join(''))
+    console.log(`Guesses remaining: ${this.guessesLeft}`)
 }
 
 const newGame = new HangmanGame('TREE HOUSE', 2)
-console.log(newGame.letters)
-newGame.GetPuzzle('t')
-console.log(newGame.guessedLetters)
-newGame.GetPuzzle('a')
-console.log(newGame.guessedLetters)
-newGame.GetPuzzle('e')
-console.log(newGame.guessedLetters)
-newGame.GetPuzzle('h')
-console.log(newGame.guessedLetters)
+newGame.GetPuzzle()
+
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    newGame.MakeGuess(guess)
+})
+
